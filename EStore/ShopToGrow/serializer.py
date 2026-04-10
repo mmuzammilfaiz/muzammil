87600e2ed26573a1da33883fe85dca7d459ea348
MuzammilFaiz2 <mmuzammilfaiz25@gmail.com>
2026-04-10 14:30:52 +0500

    validation is add in it

--- EStore/ShopToGrow/serializer.py
+++ EStore/ShopToGrow/serializer.py
@@ -5,3 +5,13 @@ class ProductSerializer(serializers.ModelSerializer):  # (better naming)
     class Meta:
         model = Product
         fields = '__all__'  
+
+    def validate_price(self,value):
+        if value < 0 :
+            raise serializers.ValidationError("Product price must be Positive")
+        return value
+    def validate_title(self,value):
+        if len(value) < 3 :
+            raise serializers.ValidationError("Title must have greater length")
+        
+        return value
